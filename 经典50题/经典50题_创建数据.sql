0227f1fdf15ff711ae8ce80056e84d927b4b6f1f
iMcYoung <108528286+iMcYoung@users.noreply.github.com>
2025-03-07 16:24:57 +0800

    Update 经典50题_创建数据.sql

--- 经典50题/经典50题_创建数据.sql
+++ 经典50题/经典50题_创建数据.sql
@@ -25,3 +25,38 @@ insert into course values
 (2,'数学',1),
 (3,'英语',3);
 
+create table teacher (
+	t_id int,
+	t_name varchar(8)
+);
+
+insert into teacher values
+(1,'张三'),
+(2,'李四'),
+(3,'王五');
+
+create table score (
+	s_id int,
+	c_id int,
+	s_score int
+);
+
+insert into score values
+(1,1,80),
+(1,2,90),
+(1,3,99),
+(2,1,70),
+(2,2,60),
+(2,3,65),
+(3,1,80),
+(3,2,80),
+(3,3,80),
+(4,1,50),
+(4,2,30),
+(4,3,40),
+(5,1,76),
+(5,2,87),
+(6,1,31),
+(6,3,34),
+(7,2,89),
+(7,3,98);
